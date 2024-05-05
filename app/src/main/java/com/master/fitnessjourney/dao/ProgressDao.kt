06937fff0111e7c882d611ce784b8e4e09e63e51
postolache-andreea-miruna <andreea.postolache@s.unibuc.ro>
2024-05-05 14:58:30 +0300

    Progress Function

--- app/src/main/java/com/master/fitnessjourney/dao/ProgressDao.kt
+++ app/src/main/java/com/master/fitnessjourney/dao/ProgressDao.kt
@@ -7,7 +7,10 @@ import androidx.room.Query
 import androidx.room.Update
 import com.master.fitnessjourney.entities.Exercice
 import com.master.fitnessjourney.entities.Progress
+import com.master.fitnessjourney.models.ExerciceByDateModel
+import com.master.fitnessjourney.models.ExerciceInProgress
 import com.master.fitnessjourney.models.ProgressWithExercices
+import java.util.Date
 
 @Dao
 public interface ProgressDao {
@@ -15,17 +18,22 @@ public interface ProgressDao {
     @Insert
     fun insertProgress(model: Progress)
 
-   // @Query("UPDATE progresses SET status= 1 WHERE id = :id")
-    //fun updateProgressStatus(id: Int): Progress?
+    @Query("UPDATE exercices_progesses SET status= 1 WHERE exerciceId = :exerciceId AND progressId = :progressId")
+    fun updateProgressStatus(exerciceId: Int, progressId: Int)
 
     @Query("DELETE FROM progresses WHERE id = :id")
     fun deleteProgress(id: Int)
 
-//    @Query("SELECT exercices.id, progresses.id,exercices.name FROM exercices " +
-//            "join exercices_progesses on(exercices.id == exercices_progesses.exerciceId)" +
-//            "join progresses on(exercices_progesses.progressId == progresses.id)" +
-//            " WHERE status = 0 ")
-//    fun getAllExercicesInProgress(): List<Exercice>
+    @Query("SELECT exercices.id 'exerciceId', progresses.id 'progressId',exercices.name FROM exercices " +
+            "join exercices_progesses on(exercices.id == exercices_progesses.exerciceId)" +
+            "join progresses on(exercices_progesses.progressId == progresses.id)" +
+            " WHERE status = 0 ")
+    fun getAllExercicesInProgress(): List<ExerciceInProgress>
 
+    @Query("SELECT exercices.id 'exerciceId', name FROM exercices " +
+            "JOIN exercices_progesses ON (exercices.id == exercices_progesses.exerciceId)" +
+            "JOIN progresses ON (progresses.id == exercices_progesses.progressId)" +
+            "WHERE status = 1 AND date = :chooseDate")
+    fun getDoneExercicesByDate(chooseDate: Date): List<ExerciceByDateModel>
 
 }
---
+++ app/src/main/java/com/master/fitnessjourney/models/ExerciceInProgress.kt
@@ -0,0 +1,7 @@
+package com.master.fitnessjourney.models
+
+data class ExerciceInProgress(
+    val exerciceId: Int,
+    val progressId: Int,
+    val name: String
+)
